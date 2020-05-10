950bb6ee4e3accd32b3abeb10902284d89da8ec4
Ale Mohamad <hello@alemohamad.com>
2020-05-10 13:57:48 +0200

    Initial Commit

---
+++ MarvelHeroes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MarvelHeroes
+//
+//  Created by Ale Mohamad on 10/05/2020.
+//  Copyright © 2020 Ale Mohamad. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
