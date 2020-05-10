8393202880a2ddec593318bfdbd11a1f153c4f04
Ale Mohamad <hello@alemohamad.com>
2020-05-10 17:23:47 +0200

    Separated cell in another view with more previews

--- MarvelHeroes/HeroesListView.swift
+++ MarvelHeroes/HeroesListView.swift
@@ -18,26 +18,7 @@ struct HeroesListView: View {
             List {
                 ForEach(0..<5) { i in
                     NavigationLink(destination: CharacterDetail()) {
-                        HStack(alignment: .top, spacing: 16.0) {
-                            Image("spiderman")
-                                .resizable()
-                                .scaledToFill()
-                                .clipShape(RoundedRectangle(cornerRadius: 10))
-                                .frame(width: 80, height: 80)
-                                .shadow(radius: 10)
-                            
-                            VStack(alignment: .leading, spacing: 8.0) {
-                                Text("Spider-Man")
-                                    .font(.headline)
-                                    .foregroundColor(.primary)
-                                
-                                Text("Bitten by a radioactive spider, high school student Peter Parker gained the speed, strength and powers of a spider. Adopting the name Spider-Man, Peter hoped to start a career using his new abilities. Taught that with great power comes great responsibility, Spidey has vowed to use his powers to help people.")
-                                    .lineLimit(3)
-                                    .font(.caption)
-                                    .foregroundColor(.secondary)
-                            }
-                        }
-                        .padding(8.0)
+                        CharacterCellView()
                     }
                 }
             }
@@ -53,8 +34,46 @@ struct HeroesListView: View {
     }
 }
 
+extension HeroesListView {
+    struct CharacterCellView: View {
+        var body: some View {
+            HStack(alignment: .top, spacing: 16.0) {
+                Image("spiderman")
+                    .resizable()
+                    .scaledToFill()
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .frame(width: 80, height: 80)
+                    .shadow(radius: 10)
+                
+                VStack(alignment: .leading, spacing: 8.0) {
+                    Text("Spider-Man")
+                        .font(.headline)
+                        .foregroundColor(.primary)
+                    
+                    Text("Bitten by a radioactive spider, high school student Peter Parker gained the speed, strength and powers of a spider. Adopting the name Spider-Man, Peter hoped to start a career using his new abilities. Taught that with great power comes great responsibility, Spidey has vowed to use his powers to help people.")
+                        .lineLimit(3)
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                }
+            }
+            .padding(8.0)
+        }
+    }
+}
+
 struct HeroesListView_Previews: PreviewProvider {
     static var previews: some View {
-        HeroesListView()
+        Group {
+            HeroesListView()
+            
+            HeroesListView()
+                .background(Color(.systemBackground))
+                .environment(\.colorScheme, .dark)
+                .previewDisplayName("Dark Mode")
+            
+            HeroesListView.CharacterCellView()
+                .previewLayout(.sizeThatFits)
+                .previewDisplayName("List character")
+        }
     }
 }
